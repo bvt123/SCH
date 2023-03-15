b8751ae3c745fe9c19810b429bdcbcba752477c3
Boris Tyshkevich <bvt@itoolabs.com>
2023-03-15 18:15:51 +0100

    systemViews

---
+++ sql/systemViews.sql
@@ -0,0 +1,9 @@
+
+CREATE DICTIONARY SCH.systemViews on cluster replicated
+(
+    `name` String,
+    `create` String
+)
+PRIMARY KEY name
+SOURCE(CLICKHOUSE(USER 'dict' PASSWORD '[HIDDEN]' QUERY '\r\n    select database || \'.\' || table as name,as_select as create from system.tables where engine=\'View\' and {condition}\r\n'))
+LAYOUT(COMPLEX_KEY_DIRECT);
